01d5c551076f39808777eb03b17b66095a880a0f
ECasielles <flapspoiler@gmail.com>
2018-02-22 14:14:05 +0100

    2nd

---
+++ app/src/main/java/com/example/usuario/ad_ejercicios_t5/SitesAdapter.java
@@ -0,0 +1,24 @@
+package com.example.usuario.ad_ejercicios_t5;
+
+import java.util.ArrayList;
+
+public class SitesAdapter {
+
+    public void setSites(ArrayList<Site> sites) {
+//set all the sites
+    }
+    public Site getAt(int position){
+//get the site in the position
+        return new Site();
+    }
+    public void add(Site site) {
+//add a site
+    }
+    public void modifyAt(Site site, int position) {
+//modify a site in the position
+    }
+    public void removeAt(int position) {
+//delete a site in the position
+    }
+
+}
